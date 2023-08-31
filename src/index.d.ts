7be58ac806bc6d0d59fa3fb6032f06fef872b75e
Jason Caldwell <jaswrks@o5p.me>
2023-08-31 15:38:45 -0400

    Project update. [p][robotic]

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 export * from './router.js';
-export { default as lazy, ErrorBoundary } from './lazy.js';
 export { default as hydrate } from './hydrate.js';
 export { default as prerender } from './prerender.js';
+export { ErrorBoundary, lazyComponent } from './lazy.js';
